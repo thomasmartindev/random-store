7c4f0e6010482582b3ae427d94a655680c46891e
Thomas Martin <thomasmdevelopment@gmail.com>
2023-02-07 13:07:30 +1000

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
-export const App = () => 'random-store'
+import { Header } from './components/Header/Header'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './components/Home'
+import { Footer } from './components/Footer'
+import { Menu } from './components/Menu/'
+import { Cart } from './components/Cart/Cart'
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route 
+          element={<Home />}
+          path='/'
+        />
+      </Routes>
+      <Footer />
+      <Menu />
+      <Cart />
+    </>
+  )
+}
