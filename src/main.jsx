3939037b5e07b2661af0c4b03dcbdf8433ff2072
Thomas Martin <thomasmdevelopment@gmail.com>
2023-02-07 13:06:40 +1000

    Add imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import Theme from './theme/Theme'
+import { GlobalStyle } from './theme/GlobalStyle'
+import { Provider } from 'react-redux'
+import store from './state/app/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
