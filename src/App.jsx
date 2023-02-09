3991b4b2339f2593248003473a2ffe570a30a14a
Thomas Martin <thomasmdevelopment@gmail.com>
2023-02-09 13:16:38 +1000

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 import { Header } from './components/Header/Header'
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './components/Home'
 import { Footer } from './components/Footer'
-import { Menu } from './components/Menu/'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './components/pages/Home'
+import { Electronics } from './components/pages/Electronics'
+import { Jewelery } from './components/pages/Jewelery'
+import { MensClothing } from './components/pages/MensClothing'
+import { WomensClothing } from './components/pages/WomensClothing'
+import { Menu } from './components/Menu'
 import { Cart } from './components/Cart/Cart'
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Footer />
       <Routes>
         <Route 
           element={<Home />}
           path='/'
         />
+        <Route 
+          element={<Electronics />}
+          path='/electronics'
+        />
+        <Route 
+          element={<Jewelery />}
+          path='/jewelery'
+        />
+        <Route 
+          element={<MensClothing />}
+          path='/mensclothing'
+        />
+        <Route 
+          element={<WomensClothing />}
+          path='/womensclothing'
+        />
       </Routes>
-      <Footer />
       <Menu />
       <Cart />
     </>
