1ab4bb7cb866ecdc9b3e85e5c328d31aa7299a7f
Thomas Martin <thomasmdevelopment@gmail.com>
2023-02-09 13:18:19 +1000

    Add reducers

--- src/state/app/store.js
+++ src/state/app/store.js
@@ -1,11 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from '../features/cartSlice'
 import isMenuOpenReducer from '../features/isMenuOpenSlice'
+import electronicsReducer from '../features/electronicsSlice'
+import jeweleryReducer from '../features/jewelerySlice'
+import mensClothingReducer from '../features/mensClothingSlice'
+import womensClothingReducer from '../features/womensClothingSlice'
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
-    isMenuOpen: isMenuOpenReducer
+    electronics: electronicsReducer,
+    isMenuOpen: isMenuOpenReducer,
+    jewelery: jeweleryReducer,
+    mensClothing: mensClothingReducer,
+    womensClothing: womensClothingReducer
   }
 })
 
