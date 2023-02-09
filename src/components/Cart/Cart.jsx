0651e301ff42cdc3bf72de50ef1be45b2c93c80a
Thomas Martin <thomasmdevelopment@gmail.com>
2023-02-09 13:33:27 +1000

    Products can now be sent to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,23 @@
 import styled from 'styled-components'
 import { useDispatch, useSelector } from 'react-redux'
 import { closeCart } from '../../state/features/cartSlice'
-import { CloseButton } from '../utilities/CloseButton'
+import { BlueButton, CloseButton, TextButton } from '../utilities/Buttons'
+import { CartItem } from './CartItem'
 
 export const Cart = () => {
   const dispatch = useDispatch()
   const isCartOpen = useSelector(state => state.cart.isCartOpen)
+  const items = useSelector(state => state.cart.items)
+
+  const sumTotal = () => {
+    let subtotal = 0
+
+    items.forEach(item => {
+      subtotal += item.price * item.quantity
+    })
+
+    return subtotal
+  }
 
   return (
     <>
@@ -14,15 +26,57 @@ export const Cart = () => {
       >
         <Wrapper>
           <Heading>
-            Shopping Bag
+            Shopping Cart
           </Heading>
           <CloseButton
-            clickHandler={() => dispatch(closeCart())}
+            onClick={() => dispatch(closeCart())}
           />
         </Wrapper>
-        <Text>
-          The Shopping Bag is empty.
-        </Text>
+        {
+          items.length 
+            ? <CartItems>
+                {
+                  items.map(item =>
+                    <CartItem
+                      id={item.id}
+                      image={item.image}
+                      key={item.id}
+                      price={item.price}
+                      quantity={item.quantity}
+                      title={item.title}
+                    />
+                  )
+                }
+              </CartItems>
+            : <Text>
+                The Shopping Cart is empty.
+              </Text>
+        }
+        {
+          items.length 
+            ? <Checkout>
+                <Subtotal>
+                  <p>
+                    Subtotal
+                  </p>
+                  <p>
+                    ${sumTotal().toFixed(2)}
+                  </p>
+                </Subtotal>
+                <p>
+                  Shipping and taxes calculated at checkout.
+                </p>
+                <BlueButton>
+                  Checkout
+                </BlueButton>
+                <TextButton
+                  onClick={() => dispatch(closeCart())}
+                >
+                  Continue Shopping
+                </TextButton>
+              </Checkout>
+            : null
+        }
       </Container>
       <Overlay
         isCartOpen={isCartOpen}
@@ -32,13 +86,36 @@ export const Cart = () => {
   )
 }
 
+const CartItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+  row-gap: 2rem;
+  width: 100%;
+`
+
+const Checkout = styled.div`
+  color: ${({ theme }) => theme.grey};
+  display: flex;
+  flex-direction: column;
+  font-size: 1.4rem;
+  padding-top: 5rem;
+  row-gap: 2rem;
+  width: 100%;
+
+  @media (max-width: 600px) {
+    padding-top: 2.5rem;
+    row-gap: 1rem;
+  }
+`
+
 const Container = styled.div`
   align-items: center;
   background-color: #FFFFFF;
   display: flex;
   flex-direction: column;
   height: 100%;
-  padding: 2.5rem;
+  padding: 2.5rem 2.5rem 5rem;
   position: fixed;
   right: ${({ isCartOpen }) => isCartOpen ? '0' : '-110%'};
   top: 0;
@@ -47,6 +124,7 @@ const Container = styled.div`
   z-index: 1;
 
   @media (max-width: 600px) {
+    padding: 2.5rem;
     width: 300px;
   }
 `
@@ -65,6 +143,14 @@ const Overlay = styled.div`
   top: 0;
   transition: left 0.75s ease-in-out;
   width: 100%;
+`
+
+const Subtotal = styled.div`
+  color: #000000;
+  display: flex;
+  font-size: 1.6rem;
+  font-weight: 600;
+  justify-content: space-between;
 `
 
 const Text = styled.p`
